60a75964d85cda9bd3ea4419a97d569a040c0f32
Neps <n3ps@users.noreply.github.com>
2015-04-27 16:36:29 -0500

    Added AutoMapper demo

---
+++ AutoMapper/AutoMapper/Objects/GetDetailsResponse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Nib.ITOps.Db.Universe.Adapter.CommandResults;
+
+namespace Nib.Sales.HttpServer.Dtos
+{
+    public class GetDetailsResponse : ICommandResult
+    {
+        public string OwnerTitle { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerMiddleName { get; set; }
+        public string OwnerLastName { get; set; }
+        public string OwnerBirthDate { get; set; }
+        public string PolicyAddress1 { get; set; }
+        public string PolicyAddress2 { get; set; }
+        public string PolicyAddress3 { get; set; }
+        public string PolicyPostCode { get; set; }
+        public string PolicyMobilePhone { get; set; }
+        public string PolicyWorkPhone { get; set; }
+        public string PolicyHomePhone { get; set; }
+        public string PolicyEmail { get; set; }
+        public string PolicyMasterGroupId { get; set; }
+        public string PolicyGroupId { get; set; }
+        public string PolicyGroupDescription { get; set; }
+        public string PolicyStatus { get; set; }
+        public string PolicyJoinDate { get; set; }
+        public string PolicyPlanId { get; set; }
+        public string PolicyPlanDescription { get; set; }
+        public string PolicyNextPayAmt { get; set; }
+        public string PolicyNextPayDate { get; set; }
+
+        public int ReturnCode
+        {
+            get;
+            set;
+        }
+
+        public string ReturnInfo
+        {
+            get;
+            set;
+        }
+
+        public string Date
+        {
+            get;
+            set;
+        }
+
+        public string Time
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ AutoMapper/AutoMapper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nib.Sales.HttpServer;
+using Nib.Sales.HttpServer.Dtos;
+using Nib.Sales.Interface.Dtos.Responses;
+using NUnit.Framework;
+
+namespace AutoMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoMapperConfiguration.ConfigureMappings();
+        }
+    }
+
+    [TestFixture]
+    public class MappingTests
+    {
+        [Test]
+        public void Check()
+        {
+            AutoMapperConfiguration.ConfigureMappings();
+
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+
+
+}
