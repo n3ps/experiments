122b1b08fe85882b27b6bbef572334c96aa8b258
Neps <n3ps@users.noreply.github.com>
2015-04-27 16:01:49 -0500

    Added Ninject demo

---
+++ NinjectDemo/Ninject3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninject3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel(new TestModule());
+
+            var samurai = kernel.Get<Samurai>();
+
+            samurai.Attack("soldier");
+        }
+    }
+}
---
+++ NinjectDemo/Ninject3/Samurai.cs
@@ -0,0 +1,27 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ninject3
+{
+    class Samurai
+    {
+        readonly IWeapon[] allWeapons;
+
+        [Inject]
+        public Samurai(IWeapon[] weapons)
+        {
+            this.allWeapons = weapons;
+        }
+
+        public void Attack(string target)
+        {
+            foreach (var weapon in allWeapons)
+            {
+                weapon.Hit(target);
+            }
+        }
+    }
+}
