5892cf9a5f607cac11058129de13c19f9e5e38f7
Neps <n3ps@users.noreply.github.com>
2015-04-29 14:01:18 -0500

    Remove nuget packages and add gitignore

---
+++ ASPNET/_Snippets/RequireTrueAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class RequireTrueAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return value != null && (bool)value == true;
+        }
+    }
+}
